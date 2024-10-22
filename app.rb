c56108c7dfd82c8b150570e07c56340c1d9eb170
katizab <100358952+katizab@users.noreply.github.com>
2024-10-22 16:03:54 +0000

    1

--- app.rb
+++ app.rb
@@ -3,7 +3,34 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
+  <h1>Welcome to Rock-Paper-Scissors!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+
+get("/rock") do
+
+moves = ["rock", "paper", "scissors"]
+comp_moves= moves.sample
+
+if comp_moves=="rock"
+    outcome="tied"
+elsif comp_moves=="paper"
+    outcome="lost"
+else
+    outcome="won"
+end
+
+  "
+  <h2>We played rock!</h2>
+  <h2>They played #{comp_moves}!</h2>
+  <h2>We #{outcome}!</h2>
+  "
+end
+
+get("/paper") do
+end
+
+get("/scissors") do
 end
